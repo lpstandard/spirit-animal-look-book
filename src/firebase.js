bdf92e9d153bfd7579a5c571c95f8dc428cd1a4c
Tony Kung <lps.tony@gmail.com>
2017-06-02 12:01:03 -0500

    security_rules

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
-};
-
-firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA5yoVQg2hPhO0Y2dQu8yltS2vZd2SlTAg",
+    authDomain: "social-animals-3f774.firebaseapp.com",
+    databaseURL: "https://social-animals-3f774.firebaseio.com",
+    projectId: "social-animals-3f774",
+    storageBucket: "social-animals-3f774.appspot.com",
+    messagingSenderId: "351725859956"
+  };
+  firebase.initializeApp(config);
 
 export default firebase;
 
